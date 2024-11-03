7b8a511b8d3a27bc1630606c0351dde9aefde68d
Joao Victor Lucena <jvpdlucena@gmail.com>
2024-11-03 01:06:15 -0300

    logo faceu add

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ import logo from "@/public/images/logos/logo-light-mode.svg"
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex" aria-label="FACEU">
-      <Image src={logo} alt="Logo" width={140} height={140} />
+      <Image src={logo} alt="Logo" width={150} height={150} />
     </Link>
   );
 }
