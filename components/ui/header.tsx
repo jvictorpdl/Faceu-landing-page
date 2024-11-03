7af1bd0131b98075914afe9bbc5ec803b343456e
Joao Victor Lucena <jvpdlucena@gmail.com>
2024-11-03 01:04:19 -0300

    modificacoes iniciais no header (sem enderecos ainda)

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -3,33 +3,30 @@ import Logo from "./logo";
 
 export default function Header() {
   return (
-    <header className="fixed top-2 z-30 w-full md:top-6">
+    <header className="w-full bg-white shadow-lg">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
+        <div className="flex h-16 items-center justify-between">
           {/* Site branding */}
-          <div className="flex flex-1 items-center">
-            <Logo />
+          <div className="flex items-center justify-center ">
+            <Link href="/" aria-label="Página inicial">
+              <Logo />
+            </Link>
           </div>
 
-          {/* Desktop sign in links */}
-          <ul className="flex flex-1 items-center justify-end gap-3">
-            <li>
-              <Link
-                href="/signin"
-                className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50"
-              >
-                Login
+          {/* Desktop navigation links */}
+          <nav className="flex space-x-6">
+            {[
+              { href: "#", label: "Sobre" },
+              { href: "#", label: "Ferramentas" },
+              { href: "#", label: "Pesquisas" },
+              { href: "#", label: "Documentos" },
+              { href: "#", label: "Contato" },
+            ].map((item) => (
+              <Link key={item.label} href={item.href} className="text-base font-semibold text-gray-900 hover:text-gray-600 ">
+                {item.label}
               </Link>
-            </li>
-            <li>
-              <Link
-                href="/signup"
-                className="btn-sm bg-gray-800 text-gray-200 shadow hover:bg-gray-900"
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
+            ))}
+          </nav>
         </div>
       </div>
     </header>
