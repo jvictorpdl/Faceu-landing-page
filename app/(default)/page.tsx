b176470a997231e5e9be9ef203c2c19774e3d6c0
Joao Victor <jvpdlucena@gmail.com>
2025-02-18 00:15:02 -0300

    ...

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "FACEU",
+  description: "FACEU Home",
 };
 
 import Hero from "@/components/hero-home";
 import BusinessCategories from "@/components/business-categories";
 // import FeaturesPlanet from "@/components/features-planet";
-import LargeTestimonial from "@/components/large-testimonial";
+// import LargeTestimonial from "@/components/large-testimonial";
 import Cta from "@/components/cta";
 
 export default function Home() {
